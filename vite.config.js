1d62017b973729a534526fb05cd8ecb03e416a5d
moviemaapp <coord.academica.iema@gmail.com>
2024-07-08 21:35:36 -0300

    agora1

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 
 
-const bases = {
-  development: '/',
-  production: '/moviemaapp/',
-  staging: '/moviemaapp/projetos/',
-};
-
-const currentBase = bases[process.env.NODE_ENV] || '/';
 
 export default defineConfig({
   plugins: [react()],
-  base: currentBase,
+  base: "/moviemaapp/",
 })
