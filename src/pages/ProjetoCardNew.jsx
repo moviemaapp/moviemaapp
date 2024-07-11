a85912907f5b75bb0c205dcf5d8be6e8b72544a6
moviemaapp <coord.academica.iema@gmail.com>
2024-07-10 23:27:48 -0300

    botoesok

--- src/pages/ProjetoCardNew.jsx
+++ src/pages/ProjetoCardNew.jsx
@@ -5,10 +5,10 @@ import { Link } from "react-router-dom";
 const ProjetoCardNew = ({ project_slug, title }) => {
     return (
         <>
-          <div className="col-lg-3">
+          <div className="col-lg-4" style={{ display: 'flex' }}>
         
 
-            <Link to={`/moviemaapp/projeto/${project_slug}`} class="btn-get-project-card scrollto">{title}</Link>
+          <Link to={`/moviemaapp/projeto/${project_slug}`} className="btn-get-project-card scrollto p-3" style={{ width: '100%' }}>{title}</Link>
         
 
           </div>
